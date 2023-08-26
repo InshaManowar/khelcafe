3e1bdad7a292282087dd079abbccf7294f005881
Insha Manowar <inshamanowar@Inshas-MacBook-Pro.local>
2023-08-26 11:29:53 +0530

    poll backend

--- javascript/poll-payload.js
+++ javascript/poll-payload.js
@@ -1,12 +1,30 @@
 document.addEventListener('DOMContentLoaded', function () {
     const form = document.getElementById('addPollForm');
-    const submitBtn = form.querySelector('[type="submit"]'); 
+    const submitBtn = form.querySelector('[type="submit"]');
     submitBtn.addEventListener('click', async function (event) {
-        event.preventDefault(); 
+        event.preventDefault();
         const loggedInAuthorId = localStorage.getItem('fetchedAuthorId');
         console.log('id', loggedInAuthorId);
-        const selectedContentType = localStorage.getItem('selectedContentType'); 
-        const currentDate = new Date().toISOString().substring(0, 23); 
+        const currentDate = new Date().toISOString().substring(0, 23);
+        const language = form.elements.language.textContent.trim();
+        const question = form.elements.question.value.trim();
+
+        if (!language || !question) {
+            alert('Please fill in all required fields.');
+            return;
+        }
+        async function uploadImages(files) {
+            const imageUrls = [];
+
+            for (const file of files) {
+                const storageRef = firebase.storage().ref(`images/${file.name}`);
+                await storageRef.put(file);
+                const downloadURL = await storageRef.getDownloadURL();
+                imageUrls.push(downloadURL);
+            }
+
+            return imageUrls;
+        }
 
         async function fetchAuthorDetails() {
             const authorResponse = await fetch(`https://my-app-9tpgj.ondigitalocean.app/authors/getAuthorById/${loggedInAuthorId}`);
@@ -17,11 +35,20 @@ document.addEventListener('DOMContentLoaded', function () {
         const [authorDetails, imageUrls] = await Promise.all([fetchAuthorDetails()]);
         console.log('Author Details:', authorDetails);
         console.log(pollOptions)
+        console.log('Image URLs:', imageUrls);
+        const tagInput = form.elements.tags;
+        const imageCreditInput = form.elements.imageCredit;
+        
+        const tagsArray = tagInput.value.split(',').map(tag => tag.trim());
+        const imageCredit = imageCreditInput.value.trim();
+        
+        const formattedImageCredit = imageCredit ? "Image : " + imageCredit : ""; // Add prefix if credit is present
+
 
         function populatePayload() {
             const jsonData = {
                 heading: '',
-                language: '',
+                language: form.elements.language.value,
                 verified: false,
                 author: {
                     id: authorDetails.id,
@@ -33,16 +60,16 @@ document.addEventListener('DOMContentLoaded', function () {
                     imageUrl: authorDetails.imageUrl,
                     isAdmin: false,
                 },
-                type:'',
+                type: '',
                 articleType: 'pollType',
-                imgUrl: '', 
+                imgUrl: imageUrls,
                 date: currentDate,
-                tags:[1], 
+                tags: formattedImageCredit ? [formattedImageCredit, ...tagsArray] : tagsArray,
                 description: '',
                 poll: {
                     question: form.elements.question.value,
                     options: pollOptions,
-                    optionResults:[0],
+                    optionResults: [0],
                     participants: [{ id: '', index: 0 }],
                 },
                 sendNotif: sendNotif,
@@ -76,9 +103,9 @@ document.addEventListener('DOMContentLoaded', function () {
             if (response.ok) {
                 const result = await response.json();
                 console.log('Article added successfully. Response:', result);
-                
-                const insertedId = result.insertedId; 
-           
+
+                const insertedId = result.insertedId;
+
             } else {
                 console.error('Failed to add article:', response.status, response.statusText);
             }
